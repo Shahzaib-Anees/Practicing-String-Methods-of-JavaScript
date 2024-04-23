8522beda6cfffdcdf4dd0c922483e57500a70c79
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-04-23 22:25:45 +0500

    Question 17 Added

---
+++ Questions/Question17/q17.js
@@ -0,0 +1,9 @@
+let userInput = prompt("Enter a String");
+let lastIndex = userInput.length - 1;
+let lastIndexOfInput = userInput.charAt((lastIndex));
+
+document.write(`<div class="card">
+        <h4>Printing the Last Character of the UserInput</h4>
+        <span> UserInput : "${userInput}" </span>
+        <span>The last Character of ${userInput} = ${lastIndexOfInput}</span>
+        </div>`);
