5e5f9b808287c101e5949281f167daec2c425095
Shahzaib-Anees <mohammad.shahzaib529@gmail.com>
2024-04-23 22:26:33 +0500

    Question 17 Added

--- Questions/Question17/q17.js
+++ Questions/Question17/q17.js
@@ -3,7 +3,7 @@ let lastIndex = userInput.length - 1;
 let lastIndexOfInput = userInput.charAt((lastIndex));
 
 document.write(`<div class="card">
-        <h4>Printing the Last Character of the UserInput</h4>
-        <span> UserInput : "${userInput}" </span>
-        <span>The last Character of ${userInput} = ${lastIndexOfInput}</span>
-        </div>`);
+  <h4>Printing the Last Character of the UserInput</h4> 
+  <span> UserInput : "${userInput}" </span>
+  <span>The last Character of ${userInput} = ${lastIndexOfInput}</span>
+</div>`);
